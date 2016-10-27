7eee79c40b03ab9a197678006ec8e0e1919c9f77
madhaviGit <projects.madhavi@googlemail.com>
2016-10-27 10:13:59 +0200

    Modified mainactivity.java added second file

--- app/src/main/java/com/example/madhavi/pushup/MainActivity.java
+++ app/src/main/java/com/example/madhavi/pushup/MainActivity.java
@@ -9,11 +9,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Secondfile d=new Secondfile();
+        d.s=10;
     }
 
     private void Testgithub()
     {
         int i=1+1;
+        int ii=2+2;
+        int iii=3+3+3;
     }
 
 
---
+++ app/src/main/java/com/example/madhavi/pushup/Secondfile.java
@@ -0,0 +1,9 @@
+package com.example.madhavi.pushup;
+
+/**
+ * Created by Madhavi on 27.10.2016.
+ */
+public class Secondfile {
+    int s;
+
+}
