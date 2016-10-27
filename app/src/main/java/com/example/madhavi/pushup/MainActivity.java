da03da1428f7eb70b64949c11cad25c434b6b187
madhaviGit <projects.madhavi@googlemail.com>
2016-10-27 10:02:32 +0200

    Initial commit

---
+++ app/src/main/java/com/example/madhavi/pushup/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.madhavi.pushup;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    private void Testgithub()
+    {
+        int i=1+1;
+    }
+
+
+}
